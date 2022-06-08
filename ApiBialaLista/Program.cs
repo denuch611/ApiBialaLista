509569a435036da308d440e601b737ce7bd038ca
denuch611 <mdenuszek52@gmail.com>
2022-06-08 02:33:02 +0200

    Add project files.

---
+++ ApiBialaLista/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Text;
+using System.Text.Json;
+using System.IO;
+
+namespace ApiBialaLista
+{
+    class Program
+    {
+        static void Main(string[] args)
+     
+        {
+            var url = "https://wl-api.mf.gov.pl";
+            var nip = "9721290903";
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(url);
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            HttpResponseMessage response = client.GetAsync($"/api/search/nip/{nip}?date={DateTime.Now.ToString("yyyy-MM-dd")}").Result; 
+            if (response.IsSuccessStatusCode)
+            {
+                string products = response.Content.ReadAsStringAsync().Result;
+                Console.WriteLine(products);
+            }
+            Console.WriteLine(response);
+         
+        }
+
+    }
+}
